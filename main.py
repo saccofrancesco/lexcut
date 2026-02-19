e2fa1c5683f1a253851312a3804ebc60579defb0
Francesco Sacco <francescosacco.github@gmail.com>
2026-02-19 23:18:03 +0100

    Refactor MainWindow class for improved button handling and title management

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
-# Importing PyQt6 essential widgets
+# Importing libraries
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from random import choice
+
+# Storing random window titles to use with random.choice()
+window_titles: list[str] = [
+    "My App",
+    "Still My App",
+    "What on earth",
+    "This is surprising",
+    "Something went wrong",
+]
 
 
-# Creating the MainWindow class, subclassing the default one
+# Subclassing the default main windo class for better cutomization
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
+
+        # Saving the reference to a counter for the number of clicks
+        self.n_times_clicked: int = 0
 
         self.setWindowTitle("My App")
 
         self.button: QPushButton = QPushButton("Press Me!")
         self.button.clicked.connect(self.the_button_was_clicked)
 
+        # Connect the SIGNAL from the change of title of the window to a new SLOT
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+
         self.setCentralWidget(self.button)
 
-    # SLOT method to connect to the cliecked SIGNAL by the class' button
+    # SLOT method for using the button clicked evet to trigger a new random choice
+    # for the app window title
     def the_button_was_clicked(self) -> None:
+        print("Clicked.")
 
-        # When triggering the SIGNAL, uses the saved button instance in self
-        # to access it, chancking it's label and disabling it, state-wise
-        self.button.setText("You already clicked me.")
-        self.button.setEnabled(False)
+        # Choosing randomly a new title
+        new_window_title: str = choice(window_titles)
 
-        # Also changing the window title, always using a class setter method
-        self.setWindowTitle("A new window title")
+        # Outputting that the changed has been applied and what is the new title
+        print(f"Setting title: {new_window_title}")
+        self.setWindowTitle(new_window_title)
+
+    # SLOT method to check if the title is equal to something and then changing the
+    # state of the referenced button with self based on the result of the check
+    def the_window_title_changed(self, window_title: str) -> None:
+        print(f"Window title changed: {window_title}")
+
+        if window_title == "Something went wrong":
+            self.button.setDisabled(True)
 
 
 app: QApplication = QApplication([])
