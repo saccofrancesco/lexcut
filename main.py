64420f988b1389b9af9590073e84604830e69ce2
Francesco Sacco <francescosacco.github@gmail.com>
2026-02-19 23:14:58 +0100

    Refactor main.py to include QPushButton in MainWindow subclass for improved UI interaction

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+# Importing common Qt widgets
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass the QMainWindow  to customize it's aspect
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Set the title on the subclassed MainWindow
+        self.setWindowTitle("My App")
+
+        button: QPushButton = QPushButton("Press Me!")
+
+        # Set the central widget of the window instance
+        self.setCentralWidget(button)
+
 
 app: QApplication = QApplication([])
 
-window: QMainWindow = QMainWindow()
+window: MainWindow = MainWindow()
 window.show()
 
 app.exec()
