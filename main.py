39b7204a4fcd13050acc9eafc15491faf7269884
Francesco Sacco <francescosacco.github@gmail.com>
2026-02-19 23:14:19 +0100

    Refactor main.py to replace QWidget with QPushButton for simplified window creation

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# Importing Qt essentials
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QPushButton
 
-# Creating ONLY ONE instance of the Application widget
 app: QApplication = QApplication([])
 
-# Create a Qt widget, which will create a window
-window: QWidget = QWidget()
-window.show() # IMPORTANT!!! Windows are ALWAYS hidden by deafault
+window: QPushButton = QPushButton("Push Me")
+window.show()
 
-# Start the event loop
 app.exec()
-
-# Will reach this section ONLY when the MAIN window is closed
