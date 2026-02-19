5bdba65f297a540290aa3046be256cf9c79d384d
Francesco Sacco <francescosacco.github@gmail.com>
2026-02-19 23:17:11 +0100

    Refactor button click handling in MainWindow class for improved state management

--- main.py
+++ main.py
@@ -7,24 +7,29 @@ class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
+        # Create a variable where we store the current STATE of a widget, 
+        # the button in this case
+        self.button_is_checked = True
+
         self.setWindowTitle("My App")
 
         button: QPushButton = QPushButton("Press Me!")
 
         # Set the possibility for the button to be 'checked'
         button.setCheckable(True)
-
-        button.clicked.connect(self.the_button_was_clicked)
         button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
 
         # Set the button as the central widget
         self.setCentralWidget(button)
 
-    def the_button_was_clicked(self) -> None:
-        print("Clicked!")
-
     def the_button_was_toggled(self, is_checked: bool) -> None:
-        print("Checked?", is_checked)
+        
+        # Chancge the value of the STATE based on the data passed to the 
+        # SLOT by the button SIGNAL
+        self.button_is_checked = is_checked
+
+        print(self.button_is_checked)
 
 
 app: QApplication = QApplication([])
