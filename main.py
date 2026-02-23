99b85521852189bb08bd49573f5ac5ee93e6fa4b
Francesco Sacco <francescosacco.github@gmail.com>
2026-02-23 09:58:42 +0100

    Refactor MainWindow to implement a vertical layout for the Color widget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Importing the custom widget from the file
 from layout_colorwidget import Color
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -8,9 +8,16 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        # create an instance of the custom widget and pass it a color
-        widget: Color = Color("red")
+        # Create a vertical layout where to put new widgets
+        layout: QVBoxLayout = QVBoxLayout()
 
+        # Add widget to the layout
+        layout.addWidget(Color("red"))
+
+        # Creating a main widget where to apply the layout
+        widget: QWidget = QWidget()
+        widget.setLayout(layout)
+        
         self.setCentralWidget(widget)
 
 app: QApplication = QApplication([])
